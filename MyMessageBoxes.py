d6d1ed2e2149d81c6144d615070a6dfe2b06b15f
goldphantom <cameron.mcquade@outlook.com>
2024-05-01 16:55:39 +0100

    created my_message_boxes

---
+++ MyMessageBoxes.py
@@ -0,0 +1,35 @@
+from tkinter import messagebox
+
+
+class ShowMessage:
+
+    @staticmethod
+    def show_info(message):
+        messagebox.showinfo(message)
+
+    @staticmethod
+    def show_warning(message):
+        messagebox.showwarning(message)
+
+    @staticmethod
+    def show_error(message):
+        messagebox.showerror(message)
+
+    @staticmethod
+    def ask_question(message):
+        messagebox.askquestion(message)
+
+    @staticmethod
+    def ask_ok_cancel(message):
+        messagebox.askokcancel(message)
+
+    @staticmethod
+    def ask_yes_no(message):
+        messagebox.askyesno(message)
+
+    @staticmethod
+    def ask_retry(message):
+        messagebox.askretrycancel(message)
+
+
+

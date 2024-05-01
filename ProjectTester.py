72255b330977fe93659702204a994b3b302ace9e
goldphantom <cameron.mcquade@outlook.com>
2024-05-01 21:47:54 +0100

    modified my_message_boxes

---
+++ ProjectTester.py
@@ -0,0 +1,19 @@
+import unittest
+import myValidator
+
+
+class MyTestCase(unittest.TestCase):
+    def test_myValidation(self):
+        test_emails = ["test.test@test.com", "test@test.com", "@test.com", "test.com", "@", "test", ".com"]
+        # A list of seven different email formats, where the first two are valid and the remaining are invalid
+        for i in range(2):  # First two emails are checked
+            test_case = myValidator.Validator.format_checker(test_emails[1], 1)
+            self.assertEqual(test_case, True)
+            # The first two emails should return True since they are in the correct email form
+        for i in range(2, 6):  # Rest of emails are checked
+            test_case = myValidator.Validator.format_checker(test_emails[i], 1)
+            self.assertEqual(test_case, False)
+            # The last five emails should return False since they are not in the correct email form
+
+
+MyTestCase().test_myValidation()
---
+++ myValidator.py
@@ -0,0 +1,41 @@
+# Length checker
+# Format checker
+# Presence Checker
+# Data type checker
+# Password strength checker
+
+class Validator:
+    @staticmethod
+    def length_checker(info, size, choice):
+        # Choice 1: check if info == size
+        # Choice 2: check if info >= size
+        # Choice 3: check if info <= size
+        if choice == 1:
+            if len(info) == size:
+                return True
+            else:
+                return False
+        elif choice == 2:
+            if len(info) >= size:
+                return True
+            else:
+                return False
+        elif choice == 3:
+            if len(info) <= size:
+                return True
+            else:
+                return False
+        else:  # If the choice cannot be found, return False
+            return False
+
+    @staticmethod
+    def format_checker(info, choice):
+        # Choice 1: check if info is in email format
+        # Choice 2: pass#
+        if choice == 1:
+            import re
+            email_format = re.compile(r"^[A-Za-z\d]+(?:\.[A-Za-z\d]+)*@[A-Za-z\d]+(?:\.[A-Za-z\d]+)*$")
+            if re.fullmatch(email_format, info):  # Check if the info entered matches the email format
+                return True
+            else:
+                return False

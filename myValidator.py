87931b6d7123f71bfcc6b953d9e5b0a59f7a91c6
cameron <cameron.mcquade@outlook.com>
2024-05-03 15:36:21 +0100

    created password strength checker in validator

--- myValidator.py
+++ myValidator.py
@@ -1,8 +1,10 @@
+import re
 # Length checker
 # Format checker
 # Presence Checker
 # Data type checker
 # Password strength checker
+
 
 class Validator:
     @staticmethod
@@ -33,7 +35,6 @@ class Validator:
         # Choice 1: check if info is in email format
         # Choice 2: pass#
         if choice == 1:
-            import re
             email_format = re.compile(r"^[A-Za-z\d]+(?:\.[A-Za-z\d]+)*@[A-Za-z\d]+(?:\.[A-Za-z\d]+)*$")
             if re.fullmatch(email_format, info):  # Check if the info entered matches the email format
                 return True
@@ -64,3 +65,36 @@ class Validator:
                 return True
             else:
                 return False
+
+    @staticmethod
+    def password_strength_checker(info, strength):
+        #  Strength 1: Check if password is over 10 digits
+        #  Strength 2: Check if the password is over 10 digits and contains a number
+        #  Strength 3: Check if the password is over 10 digits, contains a number and a special character
+        #  Strength 4: Check if the password is over 10 digits, contains a number, a special character and a capital
+        special_characters = re.compile('[@_!#$%^&*()<>?/|}{~:]')
+        if strength == 1:
+            if len(info) >= 10:
+                return True
+            else:
+                return False
+        elif strength == 2:
+            if len(info) >= 10 and bool(re.search(r'\d', info)) is True:
+                return True
+            else:
+                return False
+        elif strength == 3:
+            if len(info) >= 10 and bool(re.search(r'\d', info)) is True and bool(
+                    special_characters.search(info)) is True:
+                return True
+            else:
+                return False
+        elif strength == 4:
+            if len(info) >= 10 and bool(re.search(r'\d', info)) is True and bool(
+                    special_characters.search(info)) is True and bool(
+                        re.match(r'\w*[A-Z]\w*', info)) is True:
+                return True
+            else:
+                return False
+        else:
+            return False

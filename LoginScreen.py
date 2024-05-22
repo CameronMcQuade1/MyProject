bfe6c5464ac8a100facf11e24ff756ab74144af4
cameron <cameron.mcquade@outlook.com>
2024-05-22 13:20:29 +0100

    saving current changes

--- LoginScreen.py
+++ LoginScreen.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, font
+from tkinter import font
 import MyMessageBoxes
 
 
@@ -23,19 +23,29 @@ class MainLogin:
         target.geometry('%dx%d+%d+%d' % (width, height, centre_x, centre_y))
 
     def main_frame(self):
-        login_frame = ttk.Frame(self.login_screen, borderwidth=2, relief='groove')
-        enter_user_label = ttk.Label(login_frame, text='Username:')
-        enter_username = ttk.Entry(login_frame, textvariable=self.User)
-        enter_password_label = ttk.Label(login_frame, text='Password:')
-        enter_password = ttk.Entry(login_frame, textvariable=self.Password, show='*')
-        show_password = ttk.Button(login_frame, text='Show')
-        enter_user_label.grid(row=0, column=0)
-        enter_username.grid(row=1, column=0)
-        enter_password_label.grid(row=0, column=1)
-        enter_password.grid(row=1, column=1)
-        show_password.grid(row=1, column=2)
+        self.login_frame = tk.Frame(self.login_screen, borderwidth=2, relief='groove')
+        self.enter_user_label = tk.Label(self.login_frame, text='Username:')
+        self.enter_username = tk.Entry(self.login_frame, textvariable=self.User)
+        self.enter_password_label = tk.Label(self.login_frame, text='Password:')
+        self.enter_password = tk.Entry(self.login_frame, textvariable=self.Password, show='*')
+        self.show_password = tk.Button(self.login_frame, text="👁")
+        self.show_password['font'] = font.Font(size=11)
+        self.enter_user_label.grid(row=0, column=0)
+        self.enter_username.grid(row=1, column=0)
+        self.enter_password_label.grid(row=0, column=1)
+        self.enter_password.grid(row=1, column=1)
+        self.show_password.grid(row=1, column=2)
+        self.show_password.bind('<ButtonPress-1>', lambda event: self.show_hide_password())
+        self.show_password.bind('<ButtonRelease-1>', lambda event: self.show_hide_password())
 
-        login_frame.pack()
+        self.login_frame.pack()
+
+
+    def show_hide_password(self):
+        if self.enter_password.cget('show') == '':
+            self.enter_password.config(show='*')
+        else:
+            self.enter_password.config(show='')
 
     def menu_bar(self):
         main_bar = tk.Menu()
@@ -49,28 +59,27 @@ class MainLogin:
         main_bar.add_cascade(label='Help', menu=help_menu)
 
     def frequently_asked_questions_gui(self):
-        faq_screen = tk.Toplevel()
+        faq_screen = tk.Toplevel(background='grey')
         faq_screen.title("FAQ:")
-        self.centre_window(faq_screen, 650, 125)
         self.exit_button(faq_screen)
-        faq_frame = ttk.Frame(faq_screen)
-        questions_and_answers = ttk.Label(faq_frame, text=("𝗪𝗵𝗮𝘁 𝗶𝘀 𝗺𝘆 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 𝗮𝗻𝗱 𝗣𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
+        faq_frame = tk.Frame(faq_screen)
+        questions_and_answers = tk.Label(faq_frame, text=("𝗪𝗵𝗮𝘁 𝗶𝘀 𝗺𝘆 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 𝗮𝗻𝗱 𝗣𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
                                                            "\n- Your Username and Password will be given to you when "
                                                            "your account is first created."
                                                            "\n𝗪𝗵𝗮𝘁 𝘁𝗼 𝗱𝗼 𝗶𝗳 𝗜'𝘃𝗲 𝗳𝗼𝗿𝗴𝗼𝘁𝘁𝗲𝗻 𝗺𝘆 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
                                                            "\n- Dont worry! Just click on the 'forgot password' button "
                                                            "and answer some questions to reset it."), relief='ridge',
-                                          font=20)
-        questions_and_answers.pack()
-        faq_frame.grid(row=1, column=0)
+                                          font=15)
+        questions_and_answers.grid(row=0, column=1)
+        faq_frame.grid(row=0, column=1)
         faq_screen.mainloop()
 
+    @staticmethod
+    def exit_button(parent):
+        quit_button = tk.Button(parent, text='𝗫', background='Red', command=parent.destroy, height=3)
+        quit_button['font'] = font.Font(size=14)
+        quit_button.grid(row=0, column=0)
 
-    def exit_button(self, parent):
-        quit_button = tk.Button(parent, text='𝗫', background='Red', command=parent.destroy)
-        font_size = font.Font(size=14)
-        quit_button['font'] = font_size
-        quit_button.grid(row=0, column=0, sticky='w')
 
-
-MainLogin()
+if __name__ == "__main__":
+    MainLogin()

0ac307650dbf63ce95976db38b19426f58fa2c4a
cameron <cameron.mcquade@outlook.com>
2024-05-23 10:32:50 +0100

    saving current changes

--- LoginScreen.py
+++ LoginScreen.py
@@ -4,23 +4,13 @@ import MyMessageBoxes
 
 
 class MainLogin:
-    def __init__(self):
-        self.login_screen = tk.Tk()
+    def __init__(self, parent):
+        self.login_screen = tk.Frame(parent)
         self.User = tk.StringVar()
         self.Password = tk.StringVar()
-        self.login_screen.title("Expense Tracker")
-        self.centre_window(self.login_screen, 1400, 700)
         self.main_frame()
-        self.menu_bar()
-        self.login_screen.mainloop()
+        self.login_screen.pack()
 
-    @staticmethod
-    def centre_window(target, width, height):
-        screen_width = target.winfo_screenwidth()
-        screen_height = target.winfo_screenheight()
-        centre_x = (screen_width / 2) - (width / 2)
-        centre_y = (screen_height / 2) - (height / 2)
-        target.geometry('%dx%d+%d+%d' % (width, height, centre_x, centre_y))
 
     def main_frame(self):
         self.login_frame = tk.Frame(self.login_screen, borderwidth=2, relief='groove')
@@ -40,46 +30,36 @@ class MainLogin:
 
         self.login_frame.pack()
 
-
     def show_hide_password(self):
         if self.enter_password.cget('show') == '':
             self.enter_password.config(show='*')
         else:
             self.enter_password.config(show='')
 
-    def menu_bar(self):
-        main_bar = tk.Menu()
-        self.login_screen.config(menu=main_bar)
-        settings_menu = tk.Menu(main_bar)
-        settings_menu.add_command(label='Change Background Colour')
-        main_bar.add_cascade(label='Settings', menu=settings_menu)
-        help_menu = tk.Menu(main_bar)
-        help_menu.add_command(label='Login Help')
-        help_menu.add_command(label='Frequently Asked Questions', command=self.frequently_asked_questions_gui)
-        main_bar.add_cascade(label='Help', menu=help_menu)
-
-    def frequently_asked_questions_gui(self):
+    @staticmethod
+    def frequently_asked_questions_gui():
         faq_screen = tk.Toplevel(background='grey')
         faq_screen.title("FAQ:")
-        self.exit_button(faq_screen)
         faq_frame = tk.Frame(faq_screen)
         questions_and_answers = tk.Label(faq_frame, text=("𝗪𝗵𝗮𝘁 𝗶𝘀 𝗺𝘆 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 𝗮𝗻𝗱 𝗣𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
-                                                           "\n- Your Username and Password will be given to you when "
-                                                           "your account is first created."
-                                                           "\n𝗪𝗵𝗮𝘁 𝘁𝗼 𝗱𝗼 𝗶𝗳 𝗜'𝘃𝗲 𝗳𝗼𝗿𝗴𝗼𝘁𝘁𝗲𝗻 𝗺𝘆 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
-                                                           "\n- Dont worry! Just click on the 'forgot password' button "
-                                                           "and answer some questions to reset it."), relief='ridge',
-                                          font=15)
+                                                          "\n- Your Username and Password will be given to you when "
+                                                          "your account is first created."
+                                                          "\n𝗪𝗵𝗮𝘁 𝘁𝗼 𝗱𝗼 𝗶𝗳 𝗜'𝘃𝗲 𝗳𝗼𝗿𝗴𝗼𝘁𝘁𝗲𝗻 𝗺𝘆 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱:"
+                                                          "\n- Dont worry! Just click on the 'forgot password' button "
+                                                          "and answer some questions to reset it."), relief='ridge',
+                                         font=8)
         questions_and_answers.grid(row=0, column=1)
         faq_frame.grid(row=0, column=1)
-        faq_screen.mainloop()
-
-    @staticmethod
-    def exit_button(parent):
-        quit_button = tk.Button(parent, text='𝗫', background='Red', command=parent.destroy, height=3)
+        quit_button = tk.Button(faq_screen, text='𝗫', background='Red', command=faq_screen.destroy, height=3)
         quit_button['font'] = font.Font(size=14)
         quit_button.grid(row=0, column=0)
+        faq_screen.mainloop()
+
+    def remove_login_frame(self):
+        self.login_screen.destroy()
 
 
 if __name__ == "__main__":
-    MainLogin()
+    test_root = tk.Tk()
+    login_frame = MainLogin(test_root)
+    test_root.mainloop()
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
 import MyAccountsDatabase
+import MyMessageBoxes
+import LoginScreen
+import tkinter as tk
+
+
+class MainGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Expense Tracker')
+        self.menu_bar()
+        self.show_login_screen()
+        self.centre_window(self.root, 1400, 700)
+        self.root.mainloop()
+
+    @staticmethod
+    def centre_window(target, width, height):
+        screen_width = target.winfo_screenwidth()
+        screen_height = target.winfo_screenheight()
+        centre_x = (screen_width / 2) - (width / 2)
+        centre_y = (screen_height / 2) - (height / 2)
+        target.geometry('%dx%d+%d+%d' % (width, height, centre_x, centre_y))
+
+    def menu_bar(self):
+        main_bar = tk.Menu()
+        self.root.config(menu=main_bar)
+        settings_menu = tk.Menu(main_bar)
+        settings_menu.add_command(label='Change Background Colour')
+        main_bar.add_cascade(label='Settings', menu=settings_menu)
+        help_menu = tk.Menu(main_bar)
+        help_menu.add_command(label='Login Help')
+        help_menu.add_command(label='Frequently Asked Questions',
+                              command=LoginScreen.MainLogin.frequently_asked_questions_gui)
+        main_bar.add_cascade(label='Help', menu=help_menu)
+
+    def show_login_screen(self):
+        login_screen = LoginScreen.MainLogin(self.root)
+
+
+MainGUI()

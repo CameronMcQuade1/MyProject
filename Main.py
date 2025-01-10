f458eeae3d8002e38c5a32ad2a2543d021cbf410
cameron <cameron.mcquade@outlook.com>
2025-01-10 10:17:45 +0000

    Removed tk bar, moved into MyExpenseWindow.py now with option to log out

--- Main.py
+++ Main.py
@@ -13,7 +13,6 @@ class MainApp:
         ctk.set_default_color_theme("blue")  # Optional: Set color theme
         self.root = ctk.CTk()  # Use CTk instead of Tk
         self.root.title('Expense Tracker')
-        self.menu_bar()  # Set up the menu bar
         self.centre_window(self.root, 850, 525)
         self.run_sequence()  # Start the login sequence
         self.root.mainloop()  # Start the main loop
@@ -40,15 +39,7 @@ class MainApp:
         target.geometry('%dx%d+%d+%d' % (width, height, centre_x, centre_y))
         target.resizable(False, False)
 
-    def menu_bar(self):
-        main_bar = tk.Menu()
-        self.root.config(menu=main_bar)
-        settings_menu = tk.Menu(main_bar, tearoff=False)
-        settings_menu.add_command(label='Change Background Colour')
-        main_bar.add_cascade(label='Settings', menu=settings_menu)
-        help_menu = tk.Menu(main_bar, tearoff=False)
-        help_menu.add_command(label='Frequently Asked Questions')
-        main_bar.add_cascade(label='Help', menu=help_menu)
+
 
 
 def app():
